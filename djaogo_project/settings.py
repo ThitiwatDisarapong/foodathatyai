f1ee54533a05f4a6c69d7b26591ef210021442b7
Fluke <thiti-w-at@hotmail.com>
2020-04-29 23:19:47 +0700

    initial commit

--- djaogo_project/settings.py
+++ djaogo_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -140,5 +139,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "thitiwat3054@gmail.com"
 EMAIL_HOST_PASSWORD = "bygpcyrpohbqdmhb"
-
-django_heroku.settings(locals())
